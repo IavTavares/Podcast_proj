6199d601126c97f3589fd78a4e736a58603b06d9
I T <ivo.a.v.tavares@gmail.com>
2022-07-31 22:23:46 +0100

    Minimal Working Scraper

---
+++ src/constants.py
@@ -0,0 +1,15 @@
+import os
+PROJ_PATH = os.path.abspath(os.path.curdir)
+
+folder_path_affaires = PROJ_PATH+"/data/affaires_etrangeres"
+folder_path_concordance = PROJ_PATH+"/data/concordance_des_temps"
+folder_path_culture = PROJ_PATH+"/data/culture_monde"
+folder_path_eco = PROJ_PATH+"/data/entendez_eco"
+
+# .env is located in the project root directory
+
+URL = "https://www.radiofrance.fr"
+url_affaires = "https://www.radiofrance.fr/franceculture/podcasts/affaires-etrangeres"
+url_concordance = "https://www.radiofrance.fr/franceculture/podcasts/concordance-des-temps"
+url_culture = "https://www.radiofrance.fr/franceculture/podcasts/cultures-monde"
+url_eco = "https://www.radiofrance.fr/franceculture/podcasts/entendez-vous-l-eco"
--- src/scraper.py
+++ src/scraper.py
@@ -1,25 +1,9 @@
-import requests
+
 from bs4 import BeautifulSoup
+from constants import *
 import json
-
-
-URL = "https://www.radiofrance.fr"
-url_affaires = "https://www.radiofrance.fr/franceculture/podcasts/affaires-etrangeres"
-url_concordance = "https://www.radiofrance.fr/franceculture/podcasts/concordance-des-temps"
-url_culture = "https://www.radiofrance.fr/franceculture/podcasts/cultures-monde"
-url_eco = "https://www.radiofrance.fr/franceculture/podcasts/entendez-vous-l-eco"
-
-page = requests.get(url_affaires)
-
-soup = BeautifulSoup(page, 'html.parser')
-results = soup.find_all("div", class_="CardDetails-title svelte-690u24")
-
-episode_list = []
-
-for result in results:
-    link = result.find("a")
-    episode_url = URL + link.attrs["href"]
-    episode_list.append(episode_url)
+import requests
+import re
 
 
 def episode_list(podcast_url):
@@ -33,6 +17,11 @@ def episode_list(podcast_url):
         list_ep.append(episode_url)
     return list_ep
 
+def file_name(episode_url):
+    string = re.search(r"([^\/]+$)", episode_url).group()
+    res = string.strip("-0123456789")+".mp3"
+    return res
+
 def episode_mp3_list(episode_url):
     """episode_url is an element from the list returned by episode_list function"""
     episode_page = requests.get(episode_url)# ep_list_concordance[0])
@@ -45,3 +34,18 @@ def episode_mp3_list(episode_url):
             ep_mp3_url = json_dict['@graph'][0]["mainEntity"]["contentUrl"]
             mp3_list.append(ep_mp3_url)
     return mp3_list
+
+def download_mp3(list_episodes:list, folder_path:str):
+        for episode_url in list_episodes:
+                episode_mp3 = episode_mp3_list(episode_url)[0]
+                r = requests.get(episode_mp3, allow_redirects=True)
+                name = file_name(episode_url)
+                if r.headers.get('content-type')=="audio/mpeg":
+                        with open(folder_path+"/"+name, 'wb') as f:
+                                f.write(r.content)
+                else:
+                        print(f"No audio/mpeg file was downloaded for {name}!")
+
+if __name__=="__main__":
+    ep_list_concordance = episode_list(url_concordance)
+    download_mp3(ep_list_concordance,folder_path_concordance)
