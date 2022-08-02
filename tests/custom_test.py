3845b3a9c7e89676aa1fcde1b92e818dfe413797
I T <ivo.a.v.tavares@gmail.com>
2022-08-02 22:16:39 +0100

    Added some simple tests

---
+++ tests/custom_test.py
@@ -0,0 +1,24 @@
+import requests
+import json
+import time
+from constants import *
+
+dev_server = ""
+stage_server = ""
+localhost = "localhost"
+
+podcast_list = [url_affaires,url_concordance,url_culture,url_eco]
+host = "0.0.0.0"
+header_req = {'Content-Type':'application/json'}
+
+for podcast_url in podcast_list:
+    data = {"podcast_url": podcast_url}
+    start = time.time()
+    response = requests.post(host+"/list_episodes", headers=header_req, json=data)
+    total_time = time.time()-start
+    print(response.json())
+    print("Total Time: {}".format(total_time))
+
+
+
+
