34912d08b8e5653080c3e5a189139387c6fa6559
I T <ivo.a.v.tavares@gmail.com>
2022-08-06 17:38:58 +0100

    Added useEffect in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,36 @@
-import React, {useState, useEffect} from 'react';
-// import './App.css';
+import React, {useEffect, useState} from 'react';
+
 
 function ListItem(props) {
-  // Correct! There is no need to specify the key here:
   return <li>{props.value}</li>;
 }
 
-async function podcast_list() {
-  let podcast_list;
-  const headers = { 'Content-Type': 'application/json' };
-  const res = await fetch('/list_podcasts', { method: 'GET',headers});
-  podcast_list = await res.json();
-  console.log(podcast_list);
-  return podcast_list
-}
 
 function App() {
-  // const [podcastList, setPodcastList] = useState(null);
-  // const headers = { 'Content-Type': 'application/json' };
+  const [podcastList, setPodcastsList] = useState(0);
+  const headers = { 'Content-Type': 'application/json' };
+  useEffect(() => {
+    fetch("/list_podcasts", {method: 'GET',headers})
+      .then(res => res.json())
+      .then(data => {
+        setPodcastsList(data);
+        console.log(podcastList)
+      });
+  }, []);
 
-  //       .then(data => setPodcastList(data.list_podcast))
-  //       .catch(error => console.log(error));
-
-  const podcastList = podcast_list();
-  // const listItems = podcastList.map((podcast) =>
-  //   // Correct! Key should be specified inside the array.
-  //   <ListItem value = {podcast} />);
+  console.log("2nd print:"+Promise.resolve(podcastList))
   return (
     <div className="Podcast_List">
-    {/* <p> {podcastList} </p> */}
+      <h2>Current followed Podcasts</h2>  
+      {podcastList.length ? (
+          <ul>{
+            podcastList.map(
+              (podcast) =>
+              <ListItem value = {podcast} />
+            )
+          }</ul>
+        ):<div> Loading...</div> 
+      }
     </div>
   );
 }
