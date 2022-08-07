0a2651a212df643f9c8270f6c496ec93b8caca68
I T <ivo.a.v.tavares@gmail.com>
2022-08-07 21:44:31 +0100

    Added Download mp3 resource.

--- src/resources/podcasts.py
+++ src/resources/podcasts.py
@@ -6,6 +6,34 @@ import json
 from scraper import *
 from utilities.filesystem import *
 
+class List_Podcasts(Resource):
+    """Manage the API.
+
+    Parameters
+    ----------
+    Resource : flask_restful.Resource
+
+    """
+    
+    def get(self):
+        podcast_list={}
+        success = True
+        errors = []
+        file_path = os.path.join(PROJ_PATH,"data","db","podcast_list.json")
+        try:
+            with open(file_path, "r") as file:
+                podcast_list = json.load(file)
+        except:
+            success = False
+            errors.append("Could not load json file with podcast_list")
+        # podcast_list is imported from constants
+        podcast_list_names = [podcast["name"] for podcast in podcast_list.values()]
+        
+        response = {'list_podcast' : podcast_list_names,
+                    'success': success,
+                    'errors': errors}
+        print(response)
+        return response
 
 class List_Episodes(Resource):
     """Manage the API.
@@ -39,11 +67,12 @@ class List_Episodes(Resource):
         response = {'list_episodes_url': list_episodes_url,
                     'list_episodes_names' : list_episodes_names,
                     'success': success,
-                    'podcast_url': podcast_url, 'errors': errors}
+                    'podcast_name': podcast_name, 'errors': errors}
         return response
 
 
-class List_Podcasts(Resource):
+
+class Download_Episodes(Resource):
     """Manage the API.
 
     Parameters
@@ -51,23 +80,25 @@ class List_Podcasts(Resource):
     Resource : flask_restful.Resource
 
     """
+    # def __init__(self, model_extr, model_sim = None,clean_out_model=None):
+    #     self.model_extr = model_extr
+    #     self.model_sim = model_sim
+    #     self.clean_out_model = clean_out_model
     
-    def get(self):
-        podcast_list={}
-        success = True
+
+    # curl --request POST --url http://127.0.0.1:5000/download_episodes --header 'Content-Type: application/json' --data '{"episodes_url_list": ["example1", "example2"],"podcast_name": "podcast_name"}'
+    def post(self):
+        data = request.get_json() # --header 'Content-Type: application/json'
+        episodes_url_list = data["episodes_url_list"]
+        podcast_name = data["podcast_name"]
+        info = get_info_podcast(podcast_name)
+        folder_path = info["folder"]
+        download_mp3(episodes_url_list,folder_path)
+        list_episodes_names = [file_name(el) for el in episodes_url_list]
+        success = "True"
         errors = []
-        file_path = os.path.join(PROJ_PATH,"data","db","podcast_list.json")
-        try:
-            with open(file_path, "r") as file:
-                podcast_list = json.load(file)
-        except:
-            success = False
-            errors.append("Could not load json file with podcast_list")
-        # podcast_list is imported from constants
-        podcast_list_names = [podcast["name"] for podcast in podcast_list.values()]
-        
-        response = {'list_podcast' : podcast_list_names,
-                    'success': success,
-                    'errors': errors}
-        print(response)
+        response = {'podcast_name': podcast_name,
+                    'list_episodes_names' : list_episodes_names,
+                    'success': success, 'errors': errors}
         return response
+
