fb076a6c8ef2f6031a23a887556e39d11b5c325c
I T <ivo.a.v.tavares@gmail.com>
2022-08-07 18:46:28 +0100

    We now can show the episodes list. :)

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ function ListItem(props) {
 
 function App() {
   const [podcastList, setPodcastsList] = useState([]);
+  const [EpisodeList, setEpisodeList] = useState([]);
   // const headers = { 'Content-Type': 'application/json' };
 
   useEffect(() => {
@@ -15,23 +16,52 @@ function App() {
       .then(res => res.json())
       .then((data) => {
         setPodcastsList(data["list_podcast"]);
-        console.log("data: " + data);
+        // console.log("data: " + data);
         // console.log("podcastList: " + podcastList)
       })
   }, []);
   
+  function episodesList(name){
+    const body = {"podcast_name": name};
+    console.log("body: " + body);
+    fetch("/list_episodes",{method:"POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(body)
+    })
+      .then(res => res.json())
+      .then((data) => {
+        setEpisodeList(data["list_episodes_url"]);
+        console.log("episode_list_data: " + data);
+        // console.log("podcastList: " + podcastList)
+      })
+  }
+  // episodesList({name:"Cultures Monde"})
 
   return (
     <div className="Podcast_List">
       <h2>Currently followed Podcasts</h2>  
       {podcastList.length ? (
           <form>
-            <select name = "dropdown">{
+            <select name = "dropdown" id= "" multiple onChange={
+                (e) => {
+                  // console.log(e);
+                  console.log(e.target.value);
+                  episodesList(e.target.value)}}>{
               podcastList.map(
                 (podcast,index) => {
                   //  {console.log("index: " + index);}
                   //  {console.log("value: " + podcast);}
                   return <ListItem index = {index} value = {podcast} />
+
+                }
+              )
+            }</select>
+            <select name = "dropdown" multiple>{
+              EpisodeList.map(
+                (episode,index) => {
+                  //  {console.log("index: " + index);}
+                  //  {console.log("value: " + podcast);}
+                  return <ListItem index = {index} value = {episode} />
 
                 }
               )
